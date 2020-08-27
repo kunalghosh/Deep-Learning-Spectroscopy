c755b68509f4b13182dd29c86f66660cb8810afb
Kunal Ghosh <kunalghosh@users.noreply.github.com>
2020-08-27 12:13:18 +0300

    Update plot_RSE_dist.py
    also fixed where `dE` is multiplied in the numerator (outside the power) so that plots look correct.

--- plot_RSE_dist.py
+++ plot_RSE_dist.py
@@ -5,23 +5,28 @@ import seaborn as sns
 import numpy as np
 
 
-data = np.load("Y_test_pred_best_mae.npz")
-predictions = data["Y_test_pred"]
+def get_hist(name="rmse"):#alternately name could be "mse"
+    data = np.load(f"Y_test_pred_best_{name}.npz")
+    predictions = data["Y_test_pred"]
 
-y_vals = np.load("Y_vals.npz")["Y_test"]
+    y_vals = np.load("Y_vals.npz")["Y_test"]
 
-rse_vals = []
+    def relative_difference(prediction, label):
+        dE = 30/len(prediction) #how many eV's one dE is
+        numerator = np.sum(dE * np.power((label-prediction),2))
+        denominator = np.sum(dE*label)
+        return np.sqrt(numerator)/denominator
+
+    rse_vals = []
+    for idx, (pred, label) in enumerate(zip(predictions, y_vals)):
+        rse_vals.append(relative_difference(pred, label))
+
+    # plt.xticks(np.arange(0, 0.16, step=0.3))
+    plt.hist(rse_vals, bins=50)
+    plt.axvline(x=0.03, color='k')
+    plt.savefig(f"dist_{name}.pdf")
+    plt.close()
 
 
-def relative_difference(prediction, label):
-    dE = 30/len(prediction) #how many eV's one dE is
-    numerator = np.sum(np.power(dE*(label-prediction),2))
-    denominator = np.sum(dE*label)
-    return np.sqrt(numerator)/denominator
-
-
-for idx, (pred, label) in enumerate(zip(predictions, y_vals)):
-    rse_vals.append(relative_difference(pred, label))
-
-plt.hist(rse_vals, bins=50)
-plt.savefig("dist_mae.pdf")
+get_hist("rmse")
+get_hist("mae")
